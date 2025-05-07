4e9912df11189596f6f011f2dd65ebda7594c19b
torresmarcoa <marcotorresaceves@gmail.com>
2025-05-07 08:25:18 -0600

    feat/getAll-getSingle-routes

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
-const express = require('express');
+const express = require("express");
+const mongodb = require("./data/database");
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use('/', require('./routes'));
+app.use("/", require("./routes"));
 
-app.listen(port, () => {console.log(`Runing on port ${port}`)})
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port, () => {
+      console.log(`Database is listening and node running on port ${port}`);
+    });
+  }
+});
